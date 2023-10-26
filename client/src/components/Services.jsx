4590fd1853f716f5a0a917a3e80d0c24d7f36436
grandmasterkd <thewqmps@gmail.com>
2023-10-26 13:33:21 +0000

    FlytWeb Blockchain App

---
+++ client/src/components/Services.jsx
@@ -0,0 +1,191 @@
+import React, { useContext } from "react";
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+import { TransactionContext } from "../context/TransactionContext";
+import { shortenAddress } from "../utils/shortenAddress";
+import { Loader } from ".";
+
+const companyCommonStyles =
+  "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+  <input
+    placeholder={placeholder}
+    type={type}
+    step="0.0001"
+    value={value}
+    onChange={(e) => handleChange(e, name)}
+    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+  />
+);
+
+const Services = () => {
+  const {
+    currentAccount,
+    connectWallet,
+    handleChange,
+    sendTransaction,
+    formData,
+    isLoading,
+  } = useContext(TransactionContext);
+
+  const handleSubmit = (e) => {
+    const { addressTo, amount, keyword, message } = formData;
+
+    e.preventDefault();
+
+    if (!addressTo || !amount || !keyword || !message) return;
+
+    sendTransaction();
+  };
+
+  return (
+    <>
+      <div className="flex items-center ">
+        <div className="text-3xl">
+          <h4>Transactions</h4>
+          <br></br>
+          <h2 className="font-semibold">
+            Send Crypto <br></br>Across The World
+          </h2>
+        </div>
+
+        <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
+          <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
+            <div className="flex justify-between flex-col w-full h-full">
+              <div className="flex justify-between items-start">
+                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                  <SiEthereum fontSize={21} color="#fff" />
+                </div>
+                <BsInfoCircle fontSize={17} color="#fff" />
+              </div>
+              <div>
+                <p className="text-white font-light text-sm">
+                  {shortenAddress(currentAccount)}
+                </p>
+                <p className="text-white font-semibold text-lg mt-1">
+                  Ethereum
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-5">
+            <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+              <Input
+                placeholder="Address To"
+                name="addressTo"
+                type="text"
+                handleChange={handleChange}
+              />
+              <Input
+                placeholder="Amount (ETH)"
+                name="amount"
+                type="number"
+                handleChange={handleChange}
+              />
+              <Input
+                placeholder="Keyword (Gif)"
+                name="keyword"
+                type="text"
+                handleChange={handleChange}
+              />
+              <Input
+                placeholder="Enter Message"
+                name="message"
+                type="text"
+                handleChange={handleChange}
+              />
+
+              <div className="h-[1px] w-full bg-gray-400 my-2" />
+
+              {isLoading ? (
+                <Loader />
+              ) : (
+                <button
+                  type="button"
+                  onClick={handleSubmit}
+                  className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
+                >
+                  Send now
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Services;
+
+/*
+{
+
+import React from "react";
+import { BsShieldFillCheck } from "react-icons/bs";
+import { BiSearchAlt } from "react-icons/bi";
+import { RiHeart2Fill } from "react-icons/ri";
+
+
+
+const ServiceCard = ({ color, title, icon, subtitle }) => (
+  <div className="flex flex-row justify-start items-start white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
+    <div
+      className={`w-10 h-10 rounded-full flex justify-center items-center ${color}`}
+    >
+      {icon}
+    </div>
+    <div className="ml-5 flex flex-col flex-1">
+      <h3 className="mt-2 text-white text-lg">{title}</h3>
+      <p className="mt-1 text-white text-sm md:w-9/12">{subtitle}</p>
+    </div>
+  </div>
+);
+
+<div className="w-96 bg-[#18ff9b] h-96 flex justify-center p-20 "></div>;
+
+const Services = () => (
+  <div className="flex w-full justify-center items-center gradient-bg-services">
+    <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
+      <div className="flex-1 flex flex-col justify-start items-start">
+        <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient ">
+          Services that we
+          <br />
+          continue to improve
+        </h1>
+        <p className="text-left my-2 text-white font-light md:w-9/12 w-11/12 text-base">
+          The best choice for buying and selling your crypto assets, with the
+          various super friendly services we offer
+        </p>
+      </div>
+
+      <div className="flex-1 flex flex-col justify-start items-center">
+        <ServiceCard
+          color="bg-[#2952E3]"
+          title="Security gurantee"
+          icon={<BsShieldFillCheck fontSize={21} className="text-white" />}
+          subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products"
+        />
+        <ServiceCard
+          color="bg-[#8945F8]"
+          title="Best exchange rates"
+          icon={<BiSearchAlt fontSize={21} className="text-white" />}
+          subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products"
+        />
+        <ServiceCard
+          color="bg-[#F84550]"
+          title="Fastest transactions"
+          icon={<RiHeart2Fill fontSize={21} className="text-white" />}
+          subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+export default Services;
+ */
---
+++ client/src/components/Trends.jsx
@@ -0,0 +1,7 @@
+const Trends = () => (
+  <div className="p-10">
+    <div className="bg-[#00ff7b] w-36 h-20 rounded-md"></div>
+  </div>
+);
+
+export default Trends;
